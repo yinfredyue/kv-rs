92ce15554b38104302ba74e38c3f8cb172a7a06c
Yue Yin <yyin5@andrew.cmu.edu>
2022-08-29 11:17:41 -0700

    Pass cli tests

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,3 +1,38 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files to myapp
+    Get { key: String },
+    Set { key : String, value : String},
+    Rm  { key : String }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Get { key } => {
+            eprintln!("unimplemented");
+            todo!()
+        },
+        Commands::Set { key, value } => {
+            eprintln!("unimplemented");
+            todo!()
+        },
+        Commands::Rm { key } => {
+            eprintln!("unimplemented");
+            todo!()
+        },
+    }
 }
---
+++ project-1/src/lib.rs
@@ -0,0 +1,19 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn set(&self, key: String, value: String) {
+        todo!()
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        todo!()
+    }
+
+    pub fn remove(&self, key: String) {
+        todo!()
+    }
+}
